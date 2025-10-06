deaba9ec13be379d0519c6fb115f85ad7257283a
Georgiy G <georgiy35@icloud.com>
2025-10-06 18:01:30 +0500

    feat:add layer alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
 import fs from 'node:fs'
+import { defineConfig } from 'vite'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, 'src/app'),
+      '@entities': path.resolve(__dirname, 'src/entities'),
+      '@features': path.resolve(__dirname, 'src/features'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@process': path.resolve(__dirname, 'src/process'),
+      '@shared': path.resolve(__dirname, 'src/shared'),
+      '@widgets': path.resolve(__dirname, 'src/widgets'),
+    },
+  },
   server: {
     port: 443,
     host: '0.0.0.0',
