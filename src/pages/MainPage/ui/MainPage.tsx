18b6b916c784efee5fa13a89487f6854e2206c87
Georgiy G <georgiy35@icloud.com>
2025-10-08 13:57:30 +0500

    feat: add MainPage

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -3,7 +3,15 @@ import './MainPage.css'
 export const MainPage = () => {
   return (
     <div className="main-page">
-      <div className="main-content"></div>
+      <div className="main-content">
+        <div className="welcome-section">
+          <img src="/images/TMK_logo.svg" alt="TMK Logo" className="tmk-logo" />
+          <h1 className="welcome-title">Добро пожаловать 👋</h1>
+          <p className="welcome-subtitle">
+            Мы поможем оформить заказ на трубную продукцию быстро и удобно
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
