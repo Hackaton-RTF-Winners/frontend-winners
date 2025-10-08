245975a1bfd08ae8d9a584cc64700315c91b7045
Nikita Serebryanskiy <nnnikserrrr@icloud.com>
2025-10-08 15:57:01 +0500

    fix: bug fix

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,12 @@
 import './MainPage.css'
 
 export const MainPage = () => {
+  const tmkLogo = `${import.meta.env.BASE_URL}images/images-Photoroom.png`
   return (
     <div className="main-page">
       <div className="main-content">
         <div className="welcome-section">
-          <img src="/images/TMK_logo.svg" alt="TMK Logo" className="tmk-logo" />
+          <img src={tmkLogo} alt="TMK Logo" className="tmk-logo" />
           <h1 className="welcome-title">Добро пожаловать 👋</h1>
           <p className="welcome-subtitle">
             Мы поможем оформить заказ на трубную продукцию быстро и удобно
